665ab51636f204d129e27e4f56832ab8d477989e
stasyaner <stasyaner@hotmail.com>
2020-01-15 02:12:52 +0100

    initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+const withStyles = require("@webdeb/next-styles");
+
+module.exports = withStyles({
+    sass: true,
+    modules: true,
+    cssLoaderOptions: {
+        localsConvention: "dashesOnly",
+    },
+});
